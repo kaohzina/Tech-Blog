8e2ed2cf9372f1273cd4fd379fbc6b03ae5fe41a
kaohzina <zinanshin.kaoher@gmail.com>
2021-11-05 15:33:21 -0500

    working posts functions

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(require('./controllers/'));
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
