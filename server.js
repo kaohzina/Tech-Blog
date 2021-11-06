948964f0da1882e01547e8ef1ac65fdc64ee29d4
kaohzina <zinanshin.kaoher@gmail.com>
2021-11-05 20:23:58 -0500

    new homepage

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const sequelize = require("./config/connection");
+const routes = require('./controllers/');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -22,14 +24,14 @@ const PORT = process.env.PORT || 3001;
 
 // const helpers = require('./utils/helpers');
 
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
