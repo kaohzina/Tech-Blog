d5ddb7cb1d369d3cc29b073a37267768fd196cea
kaohzina <zinanshin.kaoher@gmail.com>
2021-11-05 20:54:09 -0500

    login and logout functionality

--- server.js
+++ server.js
@@ -8,21 +8,19 @@ const routes = require('./controllers/');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-// app.use(session(sess));
-
-// const helpers = require('./utils/helpers');
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
